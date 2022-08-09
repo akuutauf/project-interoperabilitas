5aeb3bd1ac53305ae096b6ca5dd5cd39c5386a25
akuutauf <taufikhidayat09121@gmail.com>
2022-08-09 11:52:59 +0700

    Adding Layout For View Data Kategori and Tugas

--- resources/views/admin/kategori.blade.php
+++ resources/views/admin/kategori.blade.php
@@ -11,25 +11,79 @@
         <div class="col-sm-4">
             <div class="page-header float-left">
                 <div class="page-title">
-                    <h1>Kategori</h1>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-8">
-            <div class="page-header float-right">
-                <div class="page-title">
-                    <ol class="breadcrumb text-right">
-                        <li class="active">Kategori</li>
-                    </ol>
+                    <h1>Data Kategori</h1>
                 </div>
             </div>
         </div>
     </div>
 
-    {{-- Content For Viewing Data Kategori --}}
-    <section class="mt-3">
-        <center>
-            <h3>Konten Data Kategori</h3>
-        </center>
-    </section>
+    <div class="content mt-3">
+        <div class="animated fadeIn">
+            <div class="row">
+
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title">Tabel Data Kategori</strong>
+                        </div>
+                        <div class="card-body">
+                            <table id="bootstrap-data-table-export" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th class="bg-theme">No</th>
+                                        <th class="bg-theme">Nama kategori</th>
+                                        <th class="bg-theme">Status Kategori</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Analisis Desain Perangkat Lunak</td>
+                                        <td>Aktif</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Manajemen Proyek</td>
+                                        <td>Aktif</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Interoperabilitas</td>
+                                        <td>Aktif</td>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td>Metode dan Model Pengembangan Perangkat Lunak</td>
+                                        <td>Aktif</td>
+                                    </tr>
+                                    <tr>
+                                        <td>5</td>
+                                        <td>Keamanan Perangkat Lunak</td>
+                                        <td>Aktif</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- .animated -->
+    </div><!-- .content -->
+
+    <script src="{{ asset('vendors/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendors/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+    <script src="{{ asset('vendors/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('vendors/jszip/dist/jszip.min.js') }}"></script>
+    <script src="{{ asset('vendors/pdfmake/build/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('vendors/pdfmake/build/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
+    <script src="{{ asset('assets/js/init-scripts/data-table/datatables-init.js') }}"></script>
 @endsection
--- resources/views/admin/tugas.blade.php
+++ resources/views/admin/tugas.blade.php
@@ -11,25 +11,77 @@
         <div class="col-sm-4">
             <div class="page-header float-left">
                 <div class="page-title">
-                    <h1>Tugas</h1>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-8">
-            <div class="page-header float-right">
-                <div class="page-title">
-                    <ol class="breadcrumb text-right">
-                        <li class="active">Tugas</li>
-                    </ol>
+                    <h1>Data Tugas</h1>
                 </div>
             </div>
         </div>
     </div>
 
-    {{-- Content For Viewing Data Kategori --}}
-    <section class="mt-3">
-        <center>
-            <h3>Konten Data Kategori</h3>
-        </center>
-    </section>
+    <div class="content mt-3">
+        <div class="animated fadeIn">
+            <div class="row">
+
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title">Tabel Data Tugas</strong>
+                        </div>
+                        <div class="card-body">
+                            <table id="bootstrap-data-table-export" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th class="bg-theme">No</th>
+                                        <th class="bg-theme">Nama Tugas</th>
+                                        <th class="bg-theme">Keterangan Tugas</th>
+                                        <th class="bg-theme">Status Tugas</th>
+                                        <th class="bg-theme">Kategori Tugas</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Membuat Diagram UML</td>
+                                        <td>Tugas disesuaikan dengan project akhir</td>
+                                        <td>Dikerjakan</td>
+                                        <td>Analisis Desain Perangkat Lunak</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Mendefinisikan kebutuhan Project</td>
+                                        <td>Deadline 33 April 2022</td>
+                                        <td>Belum Dikerjakan</td>
+                                        <td>Manajemen Proyek</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Membuat API Mobile</td>
+                                        <td>Boleh menggunakan Flutter dan yang lainnya</td>
+                                        <td>Selesai</td>
+                                        <td>Interoperabilitas</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- .animated -->
+    </div><!-- .content -->
+
+    <script src="{{ asset('vendors/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendors/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+    <script src="{{ asset('vendors/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('vendors/jszip/dist/jszip.min.js') }}"></script>
+    <script src="{{ asset('vendors/pdfmake/build/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('vendors/pdfmake/build/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
+    <script src="{{ asset('assets/js/init-scripts/data-table/datatables-init.js') }}"></script>
 @endsection
