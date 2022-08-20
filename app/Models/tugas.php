640c1efebc24ecd0aef4a8b14f8aa3c5b85fb976
akuutauf <taufikhidayat09121@gmail.com>
2022-08-20 09:19:46 +0700

    Memperbaiki perubahan file

--- app/Models/tugas.php
+++ app/Models/tugas.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class tugas extends Model
+class Tugas extends Model
 {
     use HasFactory;
+    protected $guarded = [];
     protected $table = 'tugas';
     protected $primaryKey = 'id';
     protected $fillable = ['id', 'kategori_id', 'nama_tugas', 'ket_tugas', 'status_tugas'];
