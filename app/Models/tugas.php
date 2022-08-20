59676dbc2e03043fbb81367182eb4aa6e42d522c
akuutauf <taufikhidayat09121@gmail.com>
2022-08-20 09:22:44 +0700

    Memperbaiki perubahan file 3

--- app/Models/tugas.php
+++ app/Models/tugas.php
@@ -9,4 +9,12 @@ class Tugas extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    protected $table = 'tugas';
+    protected $primaryKey = 'id';
+    protected $fillable = ['id', 'kategori_id', 'nama_tugas', 'ket_tugas', 'status_tugas'];
+
+    public function kategori()
+    {
+        return $this->belongsTo(kategori::class);
+    }
 }
